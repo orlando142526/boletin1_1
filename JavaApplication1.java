cb87acdc8fb851a532ea2251ab443782e3bcbe4a
Orlando Joel Yagual Endara <oyagualendara@nautilus31.ad.danielcastelao.org>
2019-10-08 09:04:17 +0200

    primer commit

---
+++ JavaApplication1.java
@@ -0,0 +1,12 @@
+
+package boletin1_1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+ int base=4 , altura =3, area;
+ area= (base*altura/2);
+ System.out.println("area="+area);
+    }
+    
+}
